a840a2e5c34e6386b47d9ab5c4145e23e9f09204
nileshkapri <nilesh.kapri@edba.io>
2023-11-20 15:06:20 +0530

    add

--- src/components/SecondScreen.tsx
+++ src/components/SecondScreen.tsx
@@ -1,12 +1,81 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { View, Text, Linking, Button, Alert } from 'react-native';
+import { Camera, useCameraDevice, useCodeScanner } from 'react-native-vision-camera';
 
 const SecondScreen = () => {
-  return (
-    <View style={{flex:1, backgroundColor:'red'}}>
-      <Text>SecondScreen</Text>
-    </View>
-  )
-}
+  const device = useCameraDevice('back');
+  const [isCameraActive, setCameraActive] = useState(false);
+  const [hasScanned, setHasScanned] = useState(false);
 
-export default SecondScreen
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
+
+  const requestCameraPermission = async () => {
+    const permission = await Camera.requestCameraPermission();
+    if (permission === 'denied') {
+      await Linking.openSettings();
+    }
+  };
+
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: (codes:any) => {
+      if (!hasScanned) {
+                setHasScanned(true);
+                Alert.alert(`Scanned ${codes[0].value} codes!`);
+                console.log(codes[0].value)
+      }
+    }
+  })
+
+  const startCamera = () => {
+    setCameraActive(true);
+  };
+
+  const stopCamera = () => {
+    setCameraActive(false);
+    setHasScanned(false);
+  };
+
+  if (device == null) {
+    return (
+      <View>
+        <Text>No permission or camera not available</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={{ flex: 1 }}>
+        <Camera
+          style={{ flex: 1 }}
+          device={device}
+          isActive={isCameraActive}
+          enableZoomGesture
+          codeScanner={codeScanner}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 16,
+          }}>
+          <Button
+            title="Start Camera"
+            onPress={startCamera}
+            disabled={isCameraActive}
+          />
+          <Button
+            title="Stop Camera"
+            onPress={stopCamera}
+            disabled={!isCameraActive}
+          />
+        </View>
+      </View>
+    );
+  }
+};
+
+export default SecondScreen;
+
